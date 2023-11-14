dec0e86bbc0e4b4b131ef154451c4b97e210e8f8
martharoman <martha.rxman@gmail.com>
2023-11-13 18:03:44 -0600

    v2

--- coco/settings.py
+++ coco/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3rc)e=bg(3*!4m@+pc6=lzbftm-1z!%z01(276gnx45n*a^*$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
